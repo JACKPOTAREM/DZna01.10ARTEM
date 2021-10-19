7115805bd98cd087dde8ee04ab6ec51b0fc42e48
JACKPOTAREM <artemperov2@gmail.com>
2021-10-19 18:15:07 +0300

    function kvadrat(num) {     return num * num; } console.log(kvadrat(75)); //// Задача 1 Иващенко Артём
    {
        function callSum(num1, num2, num3) {
            return (num1 - num2)/num3;
        }
    
        console.log(callSum(23, 42, 52)); /// Задача 2
    }
    
    {
    
         function callDayString(day) {
             switch (day) {
                 case 1: return 'Понедельник';
                 case 2: return 'Вторник';
                 case 3: return 'Среда';
                 case 4: return 'Четверг';
                 case 5: return 'Пятница';
                 case 6: return 'Суббота';
                 case 7: return 'Воскресенье';
                 default: return 'НЕВЕРНОЕ ЗНАЧЕНИЕ';
             }
         }
        console.log(callDayString(4)); //// Задача 3
    }
    
    {
        ///let arr = ['Ваня', 'Иштван', 'Оля',];
        ///let newArr = arr;
        ///newArr.push('Петя'); /// push добавляет новый элемент в конец массива, поэтому 4
        ///console.log(arr.length); /// Задача 1 со скрина, не понимаю что от меня хотят, но длина масива 4
    
    }
    
    {
        let users = ["Ваня","Иштван"];
        users.push('Оля');
        console.log(users);
        users.splice(1,1,'Петя');
        console.log(users);
        console.log(users.shift());
        console.log(users);
        users.unshift('Маша','Паша');
        console.log(users); /// Задача 2 со скрина
    
    }
    
    let arr5 = ['Ваня', 'Иштван', 'Оля'];
        let a = arr5[1];
    arr5.splice(1,1,);
    console.log(arr5);
    console.log(a); /// Задача 3 со скрина, скорее всего неправильно
    
    {
        let str = 'Ваня,Иштван,Оля';
        let arr1 = str.split(',' )
        console.log(arr1) //// Задача 4 со скрина
    }
    
    {
        let arr6 = [4, 5, 6, 7];
        let newArr6 = []
        arr6.forEach(function (num) {
            newArr6.push(num * num);
    });
        console.log(newArr6) //// Задача 1 из последнего сообщения способ 1
    
        let newArr1 = arr6.map(function (num) {
            return num * num;
        });
        console.log(newArr1); //// Задача 1 из последнего сообщения способ 2
    }
    
    {
        let arr25 = [2, 53, 63, 43, 432];
        let sum = 0;
        let newArr25 = []
        arr25.forEach(function (num) {
            newArr25.push(num * num);
            sum += num * num;
        })
        console.log(newArr25);
        console.log(sum); /// Последняя задача
    }

---
+++ NewDJ.js
@@ -0,0 +1,86 @@
+const userInfo = {
+    name: "Вася",
+    age: 30
+}
+
+Object.entries(userInfo).forEach((entry) => console.log(entry)); /// Задача 1, в прочем трудно сказать верна ли запись, когда запись со скрина ничего не выводит, если дописать данный код, то считаю его верным.
+/// Так же нашел ошибку, то что после "Вася" на скрине не стоит ЗАПЯТАЯ которая обязательная.  Иващенко Артём
+
+
+let userInfo1 = {
+    name: "Вася",
+    age: 30,
+    "58": 'Значение свойства'
+}
+
+console.log(userInfo1[58]); //// Задача 2. Нам выводит 'Значение свойства', потому-что мы обращаемся к ключу "58" и получаем его значение.
+
+
+// let userInfo2 = {
+//     name: "Вася",
+//     age: 30
+// }
+// let user = userInfo2;
+// user.age = 45;
+// console.log(userInfo2.age); //// Задача 3. Нам выводит 45, потому-что как вы нам и объясняли, если мы перезадаем значение, то в консоль выводит последнее значение.
+
+
+let userInfo3 = {
+    name: "Вася",
+    age: 30,
+    showInfo() {
+        console.log(`${this.name}`);
+    }
+}
+let user1 = userInfo3;
+userInfo3 = null;
+user1.showInfo() //// Задача 4. Нам выводит Вася, потому-что this.name указывается на обертку name. Получается this будет равен userInfo3 и т.к мы обратились к name, нам выводит его значение.
+
+
+
+
+
+
+let userInfo4 = {
+    name: "Вася",
+    age: 30,
+}
+for (const key in userInfo4) {
+    const value = userInfo4[key];
+    console.log(value); //// Задача 5. Консоль выведет 2 сообщения, Вася и соответственно 30, потому что мы перебрали объект через for/in получили ключи и после чего вызвали значения этих ключей.
+}
+
+
+let userInfo5 = {
+    name: "Вася",
+    age: 30,
+    address: {
+        city: "Uzhhorod"
+    }
+}
+
+for (const key in userInfo5.address) {
+    console.log(userInfo5.address[key]);
+} //// Задача 6, в консоли мы получаем Ужгород, т.к мы перебрали объект через for/in, далее получили ключи и вызвали единственное значение и получили Ужгород
+
+
+let userInfo6 = {
+    name: "Вася",
+    age: 30,
+    "likes js": true
+}
+console.log(userInfo6["likes js"]); //// Задача 7. Записано было неправильно, т.к отсутствовали [] при таком виде записи ключа.
+
+
+// let userInfo7 = {
+//     name: "Вася",
+//     age: 30
+// }
+// let user7 = userInfo7;
+// user7.name = "Лена";
+// delete Object.propertyName
+// console.log(userInfo7); //// Задача 8.
+
+
+
+
